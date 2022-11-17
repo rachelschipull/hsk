4d3cc890afbb2ac341b2ad29f9e3c067e68c3139
preeformance <93346315+preeformance@users.noreply.github.com>
2022-11-16 19:55:21 -0500

    Create levelOne.js

---
+++ data/levelOne.js
@@ -0,0 +1,16 @@
+const levelOneWords = [
+  {
+    English: "I, me",
+    Pinyin: "wǒ",
+    Simplified: "我",
+    Traditional: "我"
+  },
+  {
+    English: "we, us (pl.)",
+    Pinyin: "wǒmen",
+    Simplified: "我",
+    Traditional: "我"
+  }
+  ]
+
+export default levelOneWords
