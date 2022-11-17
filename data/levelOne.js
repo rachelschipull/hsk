beeb2cf68c6cc9c0d3042355bd317c8b6487d1e6
preeformance <93346315+preeformance@users.noreply.github.com>
2022-11-16 20:12:05 -0500

    Update levelOne.js

--- data/levelOne.js
+++ data/levelOne.js
@@ -3,13 +3,15 @@ const levelOneWords = [
     English: "I, me",
     Pinyin: "wǒ",
     Simplified: "我",
-    Traditional: "我"
+    Traditional: "我",
+    Category: "pronoun"
   },
   {
     English: "we, us (pl.)",
     Pinyin: "wǒmen",
-    Simplified: "我",
-    Traditional: "我"
+    Simplified: "我们",
+    Traditional: "我們",
+    Category: "pronoun"
   }
   ]
 
